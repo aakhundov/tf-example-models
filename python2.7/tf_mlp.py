607299fc98b133d98e01e97e89eac7e681ad6203
Adnan Akhundov <adnan.akhundov@gmail.com>
2017-03-14 00:34:37 +0100

    Added Python 2.7 and 3.6 source files, GMM data, and screenshots of the best obtained K-Means and GMM results.

---
+++ python2.7/tf_mlp.py
@@ -0,0 +1,98 @@
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+LOCAL_FOLDER = "MNIST_data/"
+
+IMAGE_PIXELS = 784
+NUM_CLASSES = 10
+
+HIDDEN1_UNITS = 500
+HIDDEN2_UNITS = 300
+
+LEARNING_RATE = 1e-4
+TRAINING_STEPS = 2000
+BATCH_SIZE = 100
+
+
+def dense_layer(x, in_dim, out_dim, layer_name, act):
+    """Creates a single densely connected layer of a NN"""
+    with tf.name_scope(layer_name):
+        # layer weights corresponding to the input / output dimensions
+        weights = tf.Variable(
+            tf.truncated_normal(
+                [in_dim, out_dim], 
+                stddev=1.0 / tf.sqrt(float(out_dim))
+            ), name="weights"
+        )
+
+        # layer biases corresponding to output dimension
+        biases = tf.Variable(tf.zeros([out_dim]), name="biases")
+
+        # layer activations applied to Wx+b
+        layer = act(tf.matmul(x, weights) + biases, name="activations")
+
+    return layer
+
+
+# PREPARING DATA
+
+# downloading (on first run) and extracting MNIST data
+data = input_data.read_data_sets(LOCAL_FOLDER, one_hot=True, validation_size=0)
+
+
+# BUILDING COMPUTATIONAL GRAPH
+
+# model inputs: input pixels and targets
+input = tf.placeholder(tf.float32, [None, IMAGE_PIXELS], name="input")
+targets = tf.placeholder(tf.float32, [None, NUM_CLASSES], name="targets")
+
+# network layers: two hidden and one output
+hidden1 = dense_layer(input, IMAGE_PIXELS, HIDDEN1_UNITS, "hidden1", act=tf.nn.relu)
+hidden2 = dense_layer(hidden1, HIDDEN1_UNITS, HIDDEN2_UNITS, "hidden2", act=tf.nn.relu)
+output = dense_layer(hidden2, HIDDEN2_UNITS, NUM_CLASSES, "output", act=tf.identity)
+
+# loss function: cross-entropy with built-in 
+# (stable) computation of softmax from logits
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(
+        labels=targets, logits=output
+    )
+)
+
+# training algorithm: Adam with configurable learning rate
+train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy)
+
+# evaluation operation: ratio of correct predictions
+correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(targets, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+# RUNNING COMPUTATIONAL GRAPH
+
+# creating session
+sess = tf.InteractiveSession()
+
+# initializing trainable variables
+sess.run(tf.global_variables_initializer())
+
+# training loop
+for step in range(TRAINING_STEPS):
+    # fetching next batch of training data
+    batch_xs, batch_ys = data.train.next_batch(BATCH_SIZE)
+
+    if step % 100 == 0:
+        # reporting current accuracy of the model on every 100th batch
+        batch_accuracy = sess.run(accuracy, feed_dict={input: batch_xs, targets: batch_ys})
+        print "{0}:\tbatch accuracy {1:.2f}".format(step, batch_accuracy)
+
+    # running the training step with the fetched batch
+    sess.run(train_step, feed_dict={input: batch_xs, targets: batch_ys})
+
+# evaluating model prediction accuracy of the model on the test set
+test_accuracy = sess.run(accuracy, feed_dict={input: data.test.images, targets: data.test.labels})
+
+
+print "-------------------------------------------------"
+print "Test set accuracy: {0:.4f}".format(test_accuracy)
---
+++ python3.6/tf_kmeans.py
@@ -0,0 +1,120 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+DIMENSIONS = 2
+CLUSTERS = 10
+DATA_POINTS = 10000
+
+TRAINING_STEPS = 1000
+TOLERANCE = 0
+
+
+def generate_gmm_data(points, components, dimensions):
+    np.random.seed(10)
+
+    c_means = np.random.normal(size=[components, dimensions]) * 10
+    c_variances = np.abs(np.random.normal(size=[components, dimensions]))
+    c_weights = np.abs(np.random.normal(size=[components]))
+    c_weights /= np.sum(c_weights)
+
+    result = np.zeros((points, dimensions), dtype=np.float32)
+
+    for i in range(points):
+        comp = np.random.choice(np.array(range(10)), p=c_weights)
+        result[i] = np.random.multivariate_normal(
+            c_means[comp], np.diag(c_variances[comp])
+        )
+
+    np.random.seed()
+
+    return result, c_means, c_variances, c_weights
+
+
+def plot_raw_data(points):
+    plt.plot(points[:, 0], points[:, 1], "b.")
+    plt.show()
+
+
+def plot_clustered_data(points, c_means, c_assignments):
+    colors = cm.rainbow(np.linspace(0, 1, CLUSTERS))
+
+    for cluster, color in zip(range(CLUSTERS), colors):
+        c_points = points[c_assignments == cluster]
+        plt.plot(c_points[:, 0], c_points[:, 1], ".", color=color, zorder=0)
+        plt.plot(c_means[cluster, 0], c_means[cluster, 1], ".", color="black", zorder=1)
+
+    plt.show()
+
+
+# PREPARING DATA
+
+# generating DATA_POINTS points from a GMM with CLUSTERS components
+data, true_means, true_variances, true_weights = generate_gmm_data(DATA_POINTS, CLUSTERS, DIMENSIONS)
+
+
+# BUILDING COMPUTATIONAL GRAPH
+
+# model inputs: generated data points
+input = tf.placeholder(tf.float32, [None, DIMENSIONS])
+
+# trainable variables: clusters means
+random_points = tf.random_crop(input, [CLUSTERS, DIMENSIONS])
+means = tf.Variable(random_points, dtype=tf.float32)
+
+# E-step: recomputing cluster assignments according to the current means
+inputs_ex, means_ex = tf.expand_dims(input, 0), tf.expand_dims(means, 1)
+distances = tf.reduce_sum(tf.squared_difference(inputs_ex, means_ex), 2)
+assignments = tf.argmin(distances, 0)
+
+# M-step: relocating cluster means according to the computed assignments
+sums = tf.unsorted_segment_sum(input, assignments, CLUSTERS)
+counts = tf.reduce_sum(tf.one_hot(assignments, CLUSTERS), 0)
+means_ = tf.divide(sums, tf.expand_dims(counts, 1))
+
+# distortion measure: sum of squared distances 
+# from each point to the closest cluster mean
+distortion = tf.reduce_sum(tf.reduce_min(distances, 0))
+
+# updating the means by new values
+train_step = means.assign(means_)
+
+
+# RUNNING COMPUTATIONAL GRAPH
+
+# creating session
+sess = tf.InteractiveSession()
+
+# initializing trainable variables
+sess.run(tf.global_variables_initializer(), feed_dict={input: data})
+
+previous_assignments = None
+
+# training loop
+for step in range(TRAINING_STEPS):
+    # executing a training step and
+    # fetching evaluation information
+    distortion_measure, current_means, current_assignments, _ = sess.run(
+        [distortion, means_, assignments, train_step],
+        feed_dict={input: data}
+    )
+
+    if step > 0:
+        # computing the number of re-assignments during the step
+        re_assignments = (current_assignments != previous_assignments).sum()
+        print("{0}:\tdistortion {1:.2f}\tre-assignments {2}".format(
+            step, distortion_measure, re_assignments))
+
+        # stopping if no re-assignments occurred
+        if re_assignments <= TOLERANCE:
+            break
+    else:
+        print("{0}:\tdistortion {1:.2f}".format(
+            step, distortion_measure))
+
+    previous_assignments = current_assignments
+
+plot_raw_data(data)
+plot_clustered_data(data, current_means, current_assignments)
