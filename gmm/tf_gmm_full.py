6f507b8c673cbf56c20941941981e496a39036c6
Adnan Akhundov <adnan.akhundov@gmail.com>
2017-03-22 12:06:11 +0100

    Variances have been renamed to covariances. Session scope has been introduced.

--- gmm/tf_gmm_full.py
+++ gmm/tf_gmm_full.py
@@ -15,7 +15,7 @@ TOLERANCE = 1e-6
 # PREPARING DATA
 
 # generating DATA_POINTS points from a GMM with COMPONENTS components
-data, true_means, true_variances, true_weights, responsibilities = tf_gmm_tools.generate_gmm_data(
+data, true_means, true_covariances, true_weights, responsibilities = tf_gmm_tools.generate_gmm_data(
     NUM_POINTS, COMPONENTS, DIMENSIONS, seed=20, diagonal=False)
 
 
@@ -33,15 +33,15 @@ ln2piD = tf.constant(np.log(2 * np.pi) * DIMENSIONS, dtype=tf.float64)
 dim_means = tf.reduce_mean(input, 0)
 dim_distances = tf.squared_difference(input, tf.expand_dims(dim_means, 0))
 dim_variances = tf.reduce_sum(dim_distances, 0) / tf.cast(tf.shape(input)[0], tf.float64)
-avg_variance = tf.cast(tf.reduce_sum(dim_variances) / COMPONENTS / DIMENSIONS, tf.float64)
+avg_dim_variance = tf.cast(tf.reduce_sum(dim_variances) / COMPONENTS / DIMENSIONS, tf.float64)
 
 # default initial values of the variables
 initial_means = tf.placeholder_with_default(
     tf.gather(input, tf.squeeze(tf.multinomial(tf.ones([1, tf.shape(input)[0]]), COMPONENTS))),
     shape=[COMPONENTS, DIMENSIONS]
 )
-initial_variances = tf.placeholder_with_default(
-    tf.eye(DIMENSIONS, batch_shape=[COMPONENTS], dtype=tf.float64) * avg_variance,
+initial_covariances = tf.placeholder_with_default(
+    tf.eye(DIMENSIONS, batch_shape=[COMPONENTS], dtype=tf.float64) * avg_dim_variance,
     shape=[COMPONENTS, DIMENSIONS, DIMENSIONS]
 )
 initial_weights = tf.placeholder_with_default(
@@ -49,17 +49,17 @@ initial_weights = tf.placeholder_with_default(
     shape=[COMPONENTS]
 )
 
-# trainable variables: component means, variances, and weights
+# trainable variables: component means, covariances, and weights
 means = tf.Variable(initial_means, dtype=tf.float64)
-variances = tf.Variable(initial_variances, dtype=tf.float64)
+covariances = tf.Variable(initial_covariances, dtype=tf.float64)
 weights = tf.Variable(initial_weights, dtype=tf.float64)
 
 # E-step: recomputing responsibilities with respect to the current parameter values
 differences = tf.subtract(tf.expand_dims(input, 0), tf.expand_dims(means, 1))
-diff_times_inv_var = tf.matmul(differences, tf.matrix_inverse(variances))
-sum_sq_dist_times_inv_var = tf.reduce_sum(diff_times_inv_var * differences, 2)
-log_coefficients = tf.expand_dims(ln2piD + tf.log(tf.matrix_determinant(variances)), 1)
-log_components = -0.5 * (log_coefficients + sum_sq_dist_times_inv_var)
+diff_times_inv_cov = tf.matmul(differences, tf.matrix_inverse(covariances))
+sum_sq_dist_times_inv_cov = tf.reduce_sum(diff_times_inv_cov * differences, 2)
+log_coefficients = tf.expand_dims(ln2piD + tf.log(tf.matrix_determinant(covariances)), 1)
+log_components = -0.5 * (log_coefficients + sum_sq_dist_times_inv_cov)
 log_weighted = log_components + tf.expand_dims(tf.log(weights), 1)
 log_shift = tf.expand_dims(tf.reduce_max(log_weighted, 0), 0)
 exp_log_shifted = tf.exp(log_weighted - log_shift)
@@ -72,13 +72,13 @@ gamma_weighted = gamma / tf.expand_dims(gamma_sum, 1)
 means_ = tf.reduce_sum(tf.expand_dims(input, 0) * tf.expand_dims(gamma_weighted, 2), 1)
 differences_ = tf.subtract(tf.expand_dims(input, 0), tf.expand_dims(means_, 1))
 sq_dist_matrix = tf.matmul(tf.expand_dims(differences_, 3), tf.expand_dims(differences_, 2))
-variances_ = tf.reduce_sum(sq_dist_matrix * tf.expand_dims(tf.expand_dims(gamma_weighted, 2), 3), 1)
+covariances_ = tf.reduce_sum(sq_dist_matrix * tf.expand_dims(tf.expand_dims(gamma_weighted, 2), 3), 1)
 weights_ = gamma_sum / tf.cast(tf.shape(input)[0], tf.float64)
 
-# applying prior to the computed variances
-variances_ *= tf.expand_dims(tf.expand_dims(gamma_sum, 1), 2)
-variances_ += tf.expand_dims(tf.diag(tf.fill([DIMENSIONS], 2.0 * beta)), 0)
-variances_ /= tf.expand_dims(tf.expand_dims(gamma_sum + (2.0 * (alpha + 1.0)), 1), 2)
+# applying prior to the computed covariances
+covariances_ *= tf.expand_dims(tf.expand_dims(gamma_sum, 1), 2)
+covariances_ += tf.expand_dims(tf.diag(tf.fill([DIMENSIONS], 2.0 * beta)), 0)
+covariances_ /= tf.expand_dims(tf.expand_dims(gamma_sum + (2.0 * (alpha + 1.0)), 1), 2)
 
 # log-likelihood: objective function being maximized up to a TOLERANCE delta
 log_likelihood = tf.reduce_sum(tf.log(exp_log_shifted_sum)) + tf.reduce_sum(log_shift)
@@ -87,59 +87,57 @@ mean_log_likelihood = log_likelihood / tf.cast(tf.shape(input)[0] * tf.shape(inp
 # assigning new values to the parameters
 train_step = tf.group(
     means.assign(means_),
-    variances.assign(variances_),
+    covariances.assign(covariances_),
     weights.assign(weights_)
 )
 
 
 # RUNNING COMPUTATIONAL GRAPH
 
-# creating session
-sess = tf.InteractiveSession()
-
-# initializing trainable variables
-sess.run(
-    tf.global_variables_initializer(),
-    feed_dict={
-        input: data,
-        initial_means: data[:10],
-        # initial_variances: true_variances,
-        # initial_weights: true_weights
-    }
-)
-
-previous_likelihood = -np.inf
-
-# training loop
-for step in range(TRAINING_STEPS):
-    # executing a training step and
-    # fetching evaluation information
-    current_likelihood, _ = sess.run(
-        [mean_log_likelihood, train_step],
-        feed_dict={input: data}
+with tf.Session() as sess:
+    # initializing trainable variables
+    sess.run(
+        tf.global_variables_initializer(),
+        feed_dict={
+            input: data,
+            initial_means: data[:10],
+            # initial_covariances: true_covariances,
+            # initial_weights: true_weights
+        }
     )
 
-    if step > 0:
-        # computing difference between consecutive log-likelihoods
-        difference = np.abs(current_likelihood - previous_likelihood)
-        print("{0}:\tmean-likelihood {1:.8f}\tdifference {2}".format(
-            step, current_likelihood, difference))
+    previous_likelihood = -np.inf
 
-        # stopping if TOLERANCE was reached
-        if difference <= TOLERANCE:
-            break
-    else:
-        print("{0}:\tmean-likelihood {1:.8f}".format(
-            step, current_likelihood))
+    # training loop
+    for step in range(TRAINING_STEPS):
+        # executing a training step and
+        # fetching evaluation information
+        current_likelihood, _ = sess.run(
+            [mean_log_likelihood, train_step],
+            feed_dict={input: data}
+        )
 
-    previous_likelihood = current_likelihood
+        if step > 0:
+            # computing difference between consecutive log-likelihoods
+            difference = np.abs(current_likelihood - previous_likelihood)
+            print("{0}:\tmean-likelihood {1:.8f}\tdifference {2}".format(
+                step, current_likelihood, difference))
 
-# fetching final parameter values
-final_means = means.eval(sess)
-final_variances = variances.eval(sess)
+            # stopping if TOLERANCE was reached
+            if difference <= TOLERANCE:
+                break
+        else:
+            print("{0}:\tmean-likelihood {1:.8f}".format(
+                step, current_likelihood))
+
+        previous_likelihood = current_likelihood
+
+    # fetching final parameter values
+    final_means = means.eval(sess)
+    final_covariances = covariances.eval(sess)
 
 # plotting data and the obtained GMM
 tf_gmm_tools.plot_fitted_data(
-    data, final_means, final_variances,
-    true_means, true_variances
+    data, final_means, final_covariances,
+    true_means, true_covariances
 )
--- gmm/tf_gmm_grad.py
+++ gmm/tf_gmm_grad.py
@@ -16,7 +16,7 @@ LEARNING_RATE = 0.001
 # PREPARING DATA
 
 # generating DATA_POINTS points from a GMM with COMPONENTS components
-data, true_means, true_variances, true_weights, responsibilities = tf_gmm_tools.generate_gmm_data(
+data, true_means, true_covariances, true_weights, responsibilities = tf_gmm_tools.generate_gmm_data(
     NUM_POINTS, COMPONENTS, DIMENSIONS, seed=10, diagonal=True)
 
 
@@ -32,15 +32,15 @@ ln2piD = tf.constant(np.log(2 * np.pi) * DIMENSIONS, dtype=tf.float64)
 dim_means = tf.reduce_mean(input, 0)
 dim_distances = tf.squared_difference(input, tf.expand_dims(dim_means, 0))
 dim_variances = tf.reduce_sum(dim_distances, 0) / tf.cast(tf.shape(input)[0], tf.float64)
-avg_variance = tf.cast(tf.reduce_sum(dim_variances) / COMPONENTS / DIMENSIONS, tf.float64)
+avg_dim_variance = tf.cast(tf.reduce_sum(dim_variances) / COMPONENTS / DIMENSIONS, tf.float64)
 
 # default initial values of the variables
 initial_means = tf.placeholder_with_default(
     tf.gather(input, tf.squeeze(tf.multinomial(tf.ones([1, tf.shape(input)[0]]), COMPONENTS))),
     shape=[COMPONENTS, DIMENSIONS]
 )
-initial_variances = tf.placeholder_with_default(
-    tf.cast(tf.ones([COMPONENTS, DIMENSIONS]), tf.float64) * avg_variance,
+initial_covariances = tf.placeholder_with_default(
+    tf.cast(tf.ones([COMPONENTS, DIMENSIONS]), tf.float64) * avg_dim_variance,
     shape=[COMPONENTS, DIMENSIONS]
 )
 initial_weights = tf.placeholder_with_default(
@@ -48,15 +48,15 @@ initial_weights = tf.placeholder_with_default(
     shape=[COMPONENTS]
 )
 
-# trainable variables: component means, variances, and weights
+# trainable variables: component means, covariances, and weights
 means = tf.Variable(initial_means, dtype=tf.float64)
-variances = tf.Variable(initial_variances, dtype=tf.float64)
+covariances = tf.Variable(initial_covariances, dtype=tf.float64)
 weights = tf.Variable(initial_weights, dtype=tf.float64)
 
 # E-step: recomputing responsibilities with respect to the current parameter values
 sq_distances = tf.squared_difference(tf.expand_dims(input, 0), tf.expand_dims(means, 1))
-sum_sq_dist_times_inv_var = tf.reduce_sum(sq_distances / tf.expand_dims(variances, 1), 2)
-log_coefficients = tf.expand_dims(ln2piD + tf.reduce_sum(tf.log(variances), 1), 1)
+sum_sq_dist_times_inv_var = tf.reduce_sum(sq_distances / tf.expand_dims(covariances, 1), 2)
+log_coefficients = tf.expand_dims(ln2piD + tf.reduce_sum(tf.log(covariances), 1), 1)
 log_components = -0.5 * (log_coefficients + sum_sq_dist_times_inv_var)
 log_weighted = log_components + tf.expand_dims(tf.log(weights), 1)
 log_shift = tf.expand_dims(tf.reduce_max(log_weighted, 0), 0)
@@ -76,61 +76,59 @@ re_normalize_weights = weights.assign(tf.nn.softmax(weights))
 
 # RUNNING COMPUTATIONAL GRAPH
 
-# creating session
-sess = tf.InteractiveSession()
-
-# initializing trainable variables
-sess.run(
-    tf.global_variables_initializer(),
-    feed_dict={
-        input: data,
-        initial_means: data[:10],
-        # initial_variances: true_variances,
-        # initial_weights: true_weights
-    }
-)
-
-previous_likelihood = -np.inf
-
-# training loop
-for step in range(TRAINING_STEPS):
-    # shuffling the data before each epoch
-    np.random.shuffle(data)
-
-    # stochastic batch loop
-    for b in range(int(len(data) / BATCH_SIZE)):
-        # fetching subsequent batch from the data
-        batch = data[b * BATCH_SIZE:(b+1) * BATCH_SIZE]
-
-        # executing a training step on a fetched batch
-        sess.run(train_step, feed_dict={input: batch})
-
-        # re-normalizing weights
-        sess.run(re_normalize_weights)
-
-    # fetching evaluation information
-    current_likelihood = sess.run(
-        mean_log_likelihood,
-        feed_dict={input: data}
+with tf.Session() as sess:
+    # initializing trainable variables
+    sess.run(
+        tf.global_variables_initializer(),
+        feed_dict={
+            input: data,
+            initial_means: data[:10],
+            # initial_covariances: true_covariances,
+            # initial_weights: true_weights
+        }
     )
 
-    if step > 0:
-        # computing difference between consecutive log-likelihoods
-        difference = current_likelihood - previous_likelihood
-        print("{0}:\tmean-likelihood {1:.8f}\tdifference {2}".format(
-            step, current_likelihood, difference))
-    else:
-        print("{0}:\tmean-likelihood {1:.8f}".format(
-            step, current_likelihood))
+    previous_likelihood = -np.inf
 
-    previous_likelihood = current_likelihood
+    # training loop
+    for step in range(TRAINING_STEPS):
+        # shuffling the data before each epoch
+        np.random.shuffle(data)
 
-# fetching final parameter values
-final_means = means.eval(sess)
-final_variances = variances.eval(sess)
+        # stochastic batch loop
+        for b in range(int(len(data) / BATCH_SIZE)):
+            # fetching subsequent batch from the data
+            batch = data[b * BATCH_SIZE:(b+1) * BATCH_SIZE]
+
+            # executing a training step on a fetched batch
+            sess.run(train_step, feed_dict={input: batch})
+
+            # re-normalizing weights
+            sess.run(re_normalize_weights)
+
+        # fetching evaluation information
+        current_likelihood = sess.run(
+            mean_log_likelihood,
+            feed_dict={input: data}
+        )
+
+        if step > 0:
+            # computing difference between consecutive log-likelihoods
+            difference = current_likelihood - previous_likelihood
+            print("{0}:\tmean-likelihood {1:.8f}\tdifference {2}".format(
+                step, current_likelihood, difference))
+        else:
+            print("{0}:\tmean-likelihood {1:.8f}".format(
+                step, current_likelihood))
+
+        previous_likelihood = current_likelihood
+
+    # fetching final parameter values
+    final_means = means.eval(sess)
+    final_covariances = covariances.eval(sess)
 
 # plotting data and the obtained GMM
 tf_gmm_tools.plot_fitted_data(
-    data, final_means, final_variances,
-    true_means, true_variances
+    data, final_means, final_covariances,
+    true_means, true_covariances
 )
